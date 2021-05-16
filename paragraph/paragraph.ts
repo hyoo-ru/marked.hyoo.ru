91ea32bbb0af34a5fc860db2a966d19f89b93fc2
nin-jin <sairi-na-tenshi@ya.ru>
2021-05-16 10:53:26 +0000

    renames

--- paragraph/paragraph.ts
+++ paragraph/paragraph.ts
@@ -1,7 +1,7 @@
 namespace $ {
 
 	export let $hyoo_marked_paragraph = $mol_regexp.from([
-		{ content: $mol_regexp.repeat( $mol_regexp.byte ) },
+		{ content: $mol_regexp.repeat( $mol_regexp.char_any ) },
 		$mol_regexp.line_end,
 	])
 	
