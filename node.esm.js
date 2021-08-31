dfe0e5b31392e72535297f30194d1d7c94d8dc73
nin-jin <nin-jin@users.noreply.github.com>
2021-08-31 07:17:54 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -4507,7 +4507,7 @@ var $;
         '@': {
             mol_link_current: {
                 'true': {
-                    color: $.$mol_theme.text,
+                    color: $.$mol_theme.focus,
                     textShadow: '0 0',
                 }
             }
