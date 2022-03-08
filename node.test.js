cf023f44f515beecb712b4a1917691db4576c05a
nin-jin <nin-jin@users.noreply.github.com>
2022-03-08 07:17:01 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -7549,7 +7549,7 @@ var $;
             return "";
         }
         loading() {
-            return "lazy";
+            return "eager";
         }
     }
     $.$mol_image = $mol_image;
--- web.js
+++ web.js
@@ -7050,7 +7050,7 @@ var $;
             return "";
         }
         loading() {
-            return "lazy";
+            return "eager";
         }
     }
     $.$mol_image = $mol_image;
