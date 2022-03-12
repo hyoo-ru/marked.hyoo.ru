5f37be8b2598c24621da0c5caa13a6ae8d25c571
nin-jin <nin-jin@users.noreply.github.com>
2022-03-12 07:15:56 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -7182,6 +7182,9 @@ var $;
         hierarchy_col() {
             return "";
         }
+        minimal_width() {
+            return 0;
+        }
         sub() {
             return [
                 this.Head(),
@@ -7196,6 +7199,7 @@ var $;
         Row(id) {
             const obj = new this.$.$mol_grid_row();
             obj.minimal_height = () => this.row_height();
+            obj.minimal_width = () => this.minimal_width();
             obj.cells = () => this.cells(id);
             return obj;
         }
