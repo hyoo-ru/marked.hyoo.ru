3ce1af5c71254d8bee36607c754219ff66435f52
nin-jin <nin-jin@users.noreply.github.com>
2022-10-05 07:33:43 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -2018,6 +2018,7 @@ declare namespace $ {
         spellcheck(): boolean;
         length_max(): number;
         selection(val?: any): readonly number[];
+        bring(): void;
         Edit(): $mol_textarea_edit;
         row_numb(id: any): number;
         highlight(): string;
