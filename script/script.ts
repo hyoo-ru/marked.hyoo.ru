9039ce1ea63aeea2acbd02cec65a2e95451a1707
jin <jin@hyoo.ru>
2023-03-10 22:43:16 +0300

    tab script support

--- script/script.ts
+++ script/script.ts
@@ -1,12 +1,14 @@
 namespace $ {
 
-	const { or } = $mol_regexp
+	const { or, tab, char_any, repeat, line_end, vary } = $mol_regexp
 
 	export let $hyoo_marked_script_line = $mol_regexp.from([
-		'  ',
-		{ marker: [ '  ', or, '++', or, '--', or, '**' ] },
-		{ content: $hyoo_marked_line_content },
-		$mol_regexp.line_end,
+		vary([
+			tab,
+			[ '  ', { marker: [ '  ', or, '++', or, '--', or, '**' ] } ],
+		]),
+		{ content: repeat( char_any ) },
+		line_end,
 	])
 
 	export let $hyoo_marked_script = $mol_regexp.repeat_greedy( $hyoo_marked_script_line, 1 )
