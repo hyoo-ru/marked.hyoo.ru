397a0b0965d646d193cab8e7d9a264ef9f4cc659
nin-jin <nin-jin@users.noreply.github.com>
2023-04-06 19:25:07 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -1172,14 +1172,16 @@ declare namespace $ {
     class $mol_check extends $mol_button_minor {
         attr(): {
             mol_check_checked: boolean;
-            "aria-checked": boolean;
+            "aria-checked": string;
             role: string;
             disabled: boolean;
             tabindex: number;
             title: string;
         };
         sub(): readonly $mol_view_content[];
-        checked(val?: any): boolean;
+        checked(next?: any): boolean;
+        aria_checked(): string;
+        aria_role(): string;
         Icon(): any;
         title(): string;
         Title(): $mol_view;
@@ -1195,6 +1197,7 @@ declare namespace $.$$ {
         click(next?: Event): void;
         sub(): readonly $mol_view_content[];
         label(): readonly any[];
+        aria_checked(): string;
     }
 }
 
@@ -2477,12 +2480,30 @@ declare namespace $ {
             decoding: string;
             crossOrigin: any;
         };
+        attr(): {
+            width: number;
+            height: number;
+        };
+        event(): {
+            load: (next?: any) => any;
+        };
         minimal_width(): number;
         minimal_height(): number;
         uri(): string;
         loading(): string;
         decoding(): string;
         cors(): any;
+        natural_width(next?: any): number;
+        natural_height(next?: any): number;
+        load(next?: any): any;
+    }
+}
+
+declare namespace $.$$ {
+    class $mol_image extends $.$mol_image {
+        natural_width(next?: null): number;
+        natural_height(next?: null): number;
+        load(): void;
     }
 }
 
@@ -2495,7 +2516,7 @@ declare namespace $ {
         content(): readonly any[];
         host(): string;
         icon(): string;
-        Icon(): $mol_image;
+        Icon(): $$.$mol_image;
         title(): string;
     }
 }
@@ -2607,7 +2628,7 @@ declare namespace $ {
         sub(): readonly any[];
         active(next?: any): boolean;
         title(): string;
-        Image(): $mol_image;
+        Image(): $$.$mol_image;
         video_embed(): string;
         Frame(): $$.$mol_frame;
     }
@@ -2627,7 +2648,7 @@ declare namespace $ {
 
 declare namespace $ {
     class $mol_embed_any extends $mol_view {
-        Image(): $mol_image;
+        Image(): $$.$mol_image;
         Object(): $$.$mol_embed_native;
         Youtube(): $$.$mol_embed_youtube;
         title(): string;
