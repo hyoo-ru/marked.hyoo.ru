2b5add4ad1d5a7ddf676c747f372e34f77cef157
nin-jin <nin-jin@users.noreply.github.com>
2025-09-09 07:20:07 +0000

    Deploying to gh-pages from @ hyoo-ru/marked.hyoo.ru@4735fcba619f1d4a52ec2a702219ba2a4df89cf8 🚀

--- node.mjs
+++ node.mjs
@@ -8947,6 +8947,7 @@ var $;
 			return {
 				...(super.attr()), 
 				"src": (this.uri()), 
+				"title": (this.hint()), 
 				"alt": (this.title()), 
 				"loading": (this.loading()), 
 				"decoding": (this.decoding()), 
